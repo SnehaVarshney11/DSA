5f51092580e993e7dcea6ca95690fc9bd68f064d
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-10-09 21:54:12 +0530

    Maximum Product of Three Numbers

--- Arrays/09]_MaxChunksToMakeSorted2.java
+++ Arrays/09]_MaxChunksToMakeSorted2.java
@@ -1,5 +1,6 @@
 class MaxChunksToMakeSorted2 {
-    int rmin[] = new int[arr.length+1];
+    public int maxChunksToSorted(int[] arr) {
+        int rmin[] = new int[arr.length+1];
         rmin[arr.length] = Integer.MAX_VALUE;
         
         for(int i = arr.length-1;i>=0;i--)
@@ -13,5 +14,6 @@ class MaxChunksToMakeSorted2 {
             if(leftMax <= rmin[i+1])
                 count++;
         }
-        return count;    
+        return count;
+    }  
 }
