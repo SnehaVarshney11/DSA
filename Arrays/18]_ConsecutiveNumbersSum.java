c420c792b99beaa8afff2386ff76f53eb173bca4
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-10-11 18:40:52 +0530

    Consecutive Numbers Sum

---
+++ Arrays/18]_ConsecutiveNumbersSum.java
@@ -0,0 +1,12 @@
+class ConsecutiveNumbersSum {
+    public int consecutiveNumbersSum(int n) {
+        int count = 0, i = 1;
+        while(n > 0){
+            n = n-i;
+            if(n%i == 0)
+                count++;
+            i++;
+        }
+        return count;
+    }    
+}
