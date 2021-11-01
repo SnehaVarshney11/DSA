71c7f4266683286c6c0326cf5c035c5d05df2fe7
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-11-01 17:30:28 +0530

    Reverse Linked List II

---
+++ LinkedList/05]_ReverseLinkedListII.java
@@ -0,0 +1,19 @@
+class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode();
+        ListNode pre = dummy;
+        dummy.next = head;
+        
+        for(int i = 0; i < left-1; ++i)
+            pre = pre.next;
+        ListNode curr = pre.next;
+        
+        while(left++ < right){
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = pre.next;
+            pre.next = temp;
+        }
+        return dummy.next;
+    }    
+}
