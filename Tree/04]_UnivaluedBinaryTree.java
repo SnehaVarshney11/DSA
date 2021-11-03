581532211664a62c65b6fe73b9ddcc80d953bc73
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-11-03 20:36:20 +0530

    Univalued Binary Tree

---
+++ Tree/04]_UnivaluedBinaryTree.java
@@ -0,0 +1,14 @@
+class UnivaluedBinaryTree {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root == null)
+            return true;
+        return dfs(root, root.val);
+    }
+    private boolean dfs(TreeNode root, int val){
+        if(root == null)
+            return true;
+        if(root.val != val)
+            return false;
+        return dfs(root.left, val) && dfs(root.right, val);
+    }    
+}
