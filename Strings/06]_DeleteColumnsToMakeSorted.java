9fdbe4778801bdfe737009df9d9d850fd41a7b05
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-20 22:28:21 +0530

    Delete Columns to Make Sorted

---
+++ Strings/06]_DeleteColumnsToMakeSorted.java
@@ -0,0 +1,19 @@
+package Strings;
+
+class DeleteColumnsToMakeSorted {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        for(int c=0; c<strs[0].length(); c++)
+        {
+            for(int r=strs.length-2; r>=0; r--)
+            {
+                if(strs[r].charAt(c) > strs[r+1].charAt(c))
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }    
+}
