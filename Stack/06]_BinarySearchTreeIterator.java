5ba8c0ccf72d08bf9f20244699545ce548052d13
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-25 21:53:27 +0530

    Binary Search Tree Iterator

---
+++ Stack/06]_BinarySearchTreeIterator.java
@@ -0,0 +1,36 @@
+package Stack;
+
+import java.util.ArrayList;
+
+class TreeNode{
+    TreeNode left, right;
+    int val;
+}
+
+class BSTIterator {
+    int i;
+    ArrayList<Integer> al = new ArrayList<>(); 
+    public BSTIterator(TreeNode root) {
+        i = -1;
+        inOrder(root);
+    }
+    
+    public int next() {
+        i++;
+        int n = al.get(i);
+        return n;
+    }
+    
+    public boolean hasNext() {
+        return(i+1<al.size()) ? true : false;
+    }
+    
+    public void inOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        al.add(root.val);
+        inOrder(root.right);
+    }    
+}
